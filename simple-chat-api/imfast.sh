094d653330781a452881420da1152e778e1e5330
Scof <dev.scof.0237@gmail.com>
2024-04-27 22:44:24 +0900

    feat: github action CI/CD

--- simple-chat-api/imfast.sh
+++ simple-chat-api/imfast.sh
@@ -7,7 +7,7 @@ if [ "$1" = "run" ] ; then
 elif [ "$1" = "prod-run" ] ; then
     uvicorn main:application \
     --port 5000 \
-    --env-file prod.env \
+    --env-file .env \
     --host=0.0.0.0 \
     --log-config uvicorn_log_config.yaml
 elif [ "$1" = "test" ] ; then
